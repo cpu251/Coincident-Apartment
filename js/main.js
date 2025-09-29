04cc84b314ffd743e3dd9dd93b90314b280d166f
cpu251 <739940688@qq.com>
2025-09-29 16:17:41 +0800

    下载链接判断itch是否能打开

--- js/main.js
+++ js/main.js
@@ -39,6 +39,22 @@
                 }, 1000);
             }
         });
+        $('#download_link').click(function(e) {
+            e.preventDefault();
+            $.ajax({
+                type: 'GET',
+                url: 'https://static.itch.io/lib/jquery-ui-timepicker-addon.min.js',
+                timeout: 1500,
+                cache: false,
+                complete: function (data) {
+                    if (data.status === 200) {
+                        $('#download_link1')[0].click();
+                    } else {
+                        $('#download_link2')[0].click();
+                    }
+                }
+            });
+        });
     })(jQuery);
 
 
